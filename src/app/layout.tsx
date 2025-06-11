1642f29fb8b93d978ba1bbdc1e729d0dba073b85
arshakir <arshakir999@gmail.com>
2025-06-11 15:45:32 -0400

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Fira_Code } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] })
 const firaCode = Fira_Code({
@@ -21,6 +22,7 @@ export default function RootLayout({
       <head>
         <title>Shader Portfolio</title>
         <meta name="description" content="A portfolio of fragment shader creations" />
+        <script src="https://cdn.counter.dev/script.js" data-id="ac1f80ae-aec6-4df6-8d22-4ef3d1ab78b2" data-utcoffset="-4"></script>
       </head>
       <body className={firaCode.variable}>
         <ThemeProvider>{children}</ThemeProvider>
