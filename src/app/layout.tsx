3d2cc9f0386f410fea894b3568d2ef06e4b27e5a
arshakir <arshakir999@gmail.com>
2025-08-30 15:03:06 -0400

    switch to umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <head>
         <title>Shader Portfolio</title>
         <meta name="description" content="A portfolio of fragment shader creations" />
-        <script src="https://cdn.counter.dev/script.js" data-id="ac1f80ae-aec6-4df6-8d22-4ef3d1ab78b2" data-utcoffset="-4"></script>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="1d2cb4fb-3c31-4177-a3ea-dddec2983e20"></script>
       </head>
       <body className={firaCode.variable}>
         <ThemeProvider>{children}</ThemeProvider>
